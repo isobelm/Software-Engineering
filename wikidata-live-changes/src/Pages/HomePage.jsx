2075f3a877f1eeca953eed29f65758aaf5b4c048
brianlunch <blynch5@tcd.ie>
2020-02-19 13:44:25 +0000

    Added Bootsrap and Title Component

--- wikidata-live-changes/src/Pages/HomePage.jsx
+++ wikidata-live-changes/src/Pages/HomePage.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import TitleContainer from '../Components/TitleContainer'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class HomePage extends Component {
 	constructor(props) {
@@ -11,6 +13,10 @@ class HomePage extends Component {
 		return (
 			<div className="HomePage">
 				<p>Wikidata Live Changes</p>
+				 <TitleContainer
+                        title="Wikidata Live"
+                        subtitle="A web app to visualise recent changes to Wikidata in quasi real time."
+                    ></TitleContainer>
 			</div>
 		);
 	}
