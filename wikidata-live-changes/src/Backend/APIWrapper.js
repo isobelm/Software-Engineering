f32004a46b62ba285848993c811ac518c8b46bf7
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2020-02-22 00:31:11 +0000

    Add query functions

--- wikidata-live-changes/src/Backend/APIWrapper.js
+++ wikidata-live-changes/src/Backend/APIWrapper.js
@@ -1,17 +1,61 @@
 const API_ENDPOINT = 'https://www.wikidata.org/w/api.php'
 
-export const getUserData = async userNames => {
-  const namesString = userNames.join('|')
+/**
+ * @typedef {Object} User
+ * @property {number} userid - The user's id
+ * @property {number} name - The user's name
+ * @property {number} editcount - The number of edits the user did
+ * @property {number} recentactions - The number of actions a user did within
+ *           30 days 
+ * /
 
+/**
+ * Returns a list of 500 users who were recently active within 30 days which is
+ * sorted by the most edits
+ * @returns {User[]} - An array of user objects
+ */
+export const getMostEdits = async () => {
+  const compare = (a, b) => b.editcount - a.editcount
   const params = {
     action: 'query',
-    list: 'users',
-    ususers: namesString,
-    usprop: 'registration',
     format: 'json',
+    list: 'allusers',
+    auprop: 'editcount',
+    aulimit: 'max',
+    auwitheditsonly: '1',
+    auactiveusers: '1',
   }
+  const data = await query(params)
+  const users = data.query.allusers
+  users.sort(compare)
+  console.log(users)
+}
+
+/**
+ * Returns a list of 500 users who were recently active within 30 days which is
+ * sorted by the most recent actions
+ * @returns {User[]} - An array of user objects
+ */
+export const getMostActive = async () => {
+  const compare = (a, b) => b.recentactions - a.recentactions
+  const params = {
+    action: 'query',
+    format: 'json',
+    list: 'allusers',
+    auprop: 'editcount',
+    aulimit: 'max',
+    auwitheditsonly: '1',
+    auactiveusers: '1',
+  }
+  const data = await query(params)
+  const users = data.query.allusers
+  users.sort(compare)
+  console.log(users)
+}
+
+const query = async params => {
   const paramsString = new URLSearchParams(params).toString()
   const url = API_ENDPOINT + '?' + paramsString + '&origin=*'
-  const userData = await fetch(url).then(response => response.json())
-  return userData.query.users
+  const data = await fetch(url).then(response => response.json())
+  return data
 }
