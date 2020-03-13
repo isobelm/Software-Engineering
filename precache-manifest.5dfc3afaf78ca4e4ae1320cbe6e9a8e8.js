eb99daf44075fc4799896f563652e4a5375bfc99
brianlunch <blynch5@tcd.ie>
2020-03-13 19:29:25 +0000

    Updates

---
+++ precache-manifest.5dfc3afaf78ca4e4ae1320cbe6e9a8e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb9d5d5ae33491307ed40636b01da0d",
+    "url": "/Software-Engineering/wikidata-live-changes/index.html"
+  },
+  {
+    "revision": "3720b8e4190587851e3d",
+    "url": "/Software-Engineering/wikidata-live-changes/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a9f1371eb823a92e6293",
+    "url": "/Software-Engineering/wikidata-live-changes/static/css/main.6fc831c9.chunk.css"
+  },
+  {
+    "revision": "3720b8e4190587851e3d",
+    "url": "/Software-Engineering/wikidata-live-changes/static/js/2.b73545ef.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/Software-Engineering/wikidata-live-changes/static/js/2.b73545ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9f1371eb823a92e6293",
+    "url": "/Software-Engineering/wikidata-live-changes/static/js/main.a0f5e786.chunk.js"
+  },
+  {
+    "revision": "ab2b89044f5a730e60c0",
+    "url": "/Software-Engineering/wikidata-live-changes/static/js/runtime-main.56f1be78.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Software-Engineering/precache-manifest.b40a7f669adac41ce3555d17ca21c55e.js"
+  "/Software-Engineering/wikidata-live-changes/precache-manifest.5dfc3afaf78ca4e4ae1320cbe6e9a8e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Software-Engineering/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Software-Engineering/wikidata-live-changes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
