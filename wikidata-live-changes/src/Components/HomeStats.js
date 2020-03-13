ba2630bc2cd805820396133b6a5518b153139ada
Juuiko <mahonal@tcd.ie>
2020-03-13 15:37:15 +0000

    Added homepage stats

--- wikidata-live-changes/src/Components/HomeStats.js
+++ wikidata-live-changes/src/Components/HomeStats.js
@@ -6,7 +6,6 @@ class HomeStats extends Component {
 
 	 state = {
 		 text: 'something',
-		 page: null
 	 }
 
      buttonClickTwo = () => {
@@ -14,29 +13,22 @@ class HomeStats extends Component {
   		  url: "https://cors-anywhere.herokuapp.com/https://www.wikidata.org/wiki/Special:Statistics",
   		  type: 'GET',
   		  success: function(response) {
-  			  this.setState({
-  				  page: response,
-  			  })
-      	  	  }.bind(this)
+			  $("#div1").html($(response).find('.mw-statistics-articles'));
+	  		  $("#div2").html($(response).find('.mw-statistics-edits'));
+	  		  $("#div3").html($(response).find('.mw-statistics-users'));
+	  		  $("#div4").html($(response).find('.mw-statistics-users-active'));
+      	  	  }
   	   });
-		var foo = $(this.state.page).find('.mw-statistics-articles');
-		var foo2 = $(this.state.page).find('.mw-statistics-edits');
-		var foo3 = $(this.state.page).find('.mw-statistics-users');
-		var foo4 = $(this.state.page).find('.mw-statistics-users-active');
-		$("#div1").html(foo);
-		$("#div2").html(foo2);
-		$("#div3").html(foo3);
-		$("#div4").html(foo4);
 	}
 
 	render() {
 		return (
 			<div className="col-md-12">
 			<button type="button" className="align-bottom btn btn-outline-primary col-md-2" onClick={ this.buttonClickTwo }>Print Stats</button>
-				<div id="div1"> </div>
-				<div id="div2">Stats Loading</div>
-				<div id="div3"> </div>
-				<div id="div4"> </div>
+				<div id="div1" className="col-md-4"> </div>
+				<div id="div2" className="col-md-4">Stats Loading</div>
+				<div id="div3" className="col-md-4"> </div>
+				<div id="div4" className="col-md-4"> </div>
 			</div>
 		);
 	}
