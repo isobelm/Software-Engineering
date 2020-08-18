7d8b095b4ddc0b72659900f3a5ecc73095308264
Vitalis Wiens <vitalis.wiens@gmail.com>
2020-08-18 14:50:42 +0200

    removed double defs in PieChart.jsx

--- src/Components/PieChart.jsx
+++ src/Components/PieChart.jsx
@@ -103,26 +103,26 @@ class PieChart extends Component {
               animate={true}
               motionStiffness={90}
               motionDamping={15}
-              defs={[
-                {
-                  id: 'dots',
-                  type: 'patternDots',
-                  background: 'inherit',
-                  color: 'rgba(255, 255, 255, 0.3)',
-                  size: 4,
-                  padding: 1,
-                  stagger: true,
-                },
-                {
-                  id: 'lines',
-                  type: 'patternLines',
-                  background: 'inherit',
-                  color: 'rgba(255, 255, 255, 0.3)',
-                  rotation: -45,
-                  lineWidth: 6,
-                  spacing: 10,
-                },
-              ]}
+              // defs={[
+              //   {
+              //     id: 'dots',
+              //     type: 'patternDots',
+              //     background: 'inherit',
+              //     color: 'rgba(255, 255, 255, 0.3)',
+              //     size: 4,
+              //     padding: 1,
+              //     stagger: true,
+              //   },
+              //   {
+              //     id: 'lines',
+              //     type: 'patternLines',
+              //     background: 'inherit',
+              //     color: 'rgba(255, 255, 255, 0.3)',
+              //     rotation: -45,
+              //     lineWidth: 6,
+              //     spacing: 10,
+              //   },
+              // ]}
               isInteractive={this.state.fullGraph}
               onClick={onClick}
               tooltip={tooltip}
